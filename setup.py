bc5016f663c45bf266476e7360c7e723f905b2c5
kamaldeen91 <olatundekamaldeen@yahoo.com>
2019-10-23 00:46:29 -0500

    Update on setup

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup, find_packages
-# from simtools.Utilities.General import files_in_dir
-
 from General import files_in_dir
 
 setup(name='pqsp',
@@ -9,5 +7,4 @@ setup(name='pqsp',
       author='K. Okuneye',
       author_email='kokuneye@northwestern.edu',
       package_data={'': files_in_dir('python_QSP')},
-      # install_requires=['dtk-tools']
       )
